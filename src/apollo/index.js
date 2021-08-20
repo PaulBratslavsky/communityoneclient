0e2f96280a0663004d9d24478f810ee31a3e9660
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-08-20 01:54:45 -0500

    heh

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -8,7 +8,7 @@ import {
 
 const cache = new InMemoryCache();
 const defaultOptions = { mutate: { errorPolicy: "all" } };
-const httpLink = new HttpLink({ uri: 'http://localhost:1337/graphql' })
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URL || 'https://communityoneapi.herokuapp.com/graphql' })
 
 export const client = new ApolloClient({ 
   cache,

1739664303ec14ace59f7da64d0043aff7d13a6e
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-09-26 05:15:52 -0500

    added ability to pull data fro details page (#7)
    * added ability to pull data fro details page
    
    * added ability to pull data fro details page

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -1,39 +1,113 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import { gql, useQuery } from "@apollo/client";
+import { Container, Image, Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Avatar from "../componets/Avatar/avatar";
 import BackButton from "../componets/BackButton";
-import ProjectLikes from "../componets/ProjectLikes/projectLikes";
+// import ProjectLikes from "../componets/ProjectLikes/projectLikes";
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import { CgWebsite } from "react-icons/cg"
+import { IoDocumentText } from "react-icons/io5"
+
+const GET_PROJECT = gql`
+  query SingleProjectQuerry($id: ID!) {
+    project(id: $id) {
+      id
+      featuredImage {
+        url
+      }
+      name
+      description
+      developer {
+        firstName
+        lastName
+        youTube
+        linkedIn
+        resume
+        website
+        gitHub
+        avatarImage {
+          url
+        }
+      }
+    }
+  }
+`;
 
 export default function Details() {
   const { projectID } = useParams();
+  const { data, loading, error } = useQuery(GET_PROJECT, {
+    variables: { id: projectID },
+  });
+
+  if (loading) return <Spinner aniamtion="grow" />;
+  if (error) return <h1>Error</h1>;
+
+  const {
+    name,
+    description,
+    featuredImage,
+    developer: {
+      firstName,
+      lastName,
+      avatarImage,
+      gitHub,
+      linkedIn,
+      youTube,
+      resume,
+      website,
+    },
+  } = data.project;
+
   return (
     <Container>
       <div className="details">
-          <div className="project-details bg-secondary shadow my-3 rounded p-3">
+        <div className="project-header bg-secondary  mt-3 rounded overflow-hidden bg-primary">
+          <Image
+            src={featuredImage.url}
+            alt="name"
+            style={{ height: "100%", width: "100%", objectFit: "cover" }}
+          />
+        </div>
+        <div className="project-details bg-secondary  rounded p-3 mb-3">
+          <div>
             <header>
-              <h1>Details Page Private for blog with id {projectID}</h1>
-              <BackButton />
+              <h1>{name}</h1>
             </header>
+            <p>{description}</p>
+            <BackButton className="float-end" />
           </div>
+        </div>
 
-          <div className="user-details bg-primary shadow mt-3 rounded p-3">
-            <div className="d-flex justify-content-center align-items-center p-2">
-              <Avatar
-                size={175}
-                imgUrl="https://res.cloudinary.com/course-share/image/upload/v1629672423/6153188_57ff4c2bc0_1c63dadd23.jpg"
-                firstName={"Paul"}
-                lastName={"Brats"}
-                showName
-              />
-              
+        <div className="user-details bg-primary  mt-3 rounded p-3">
+          <div className="d-flex justify-content-center align-items-center mt-3">
+            <Avatar
+              size={175}
+              imgUrl={avatarImage.url}
+              firstName={firstName}
+              lastName={lastName}
+              showName
+            />
+           
+          </div>
+          <div className="d-flex justify-content-evenly align-items-center w-100">
+              {gitHub && <a href={gitHub} target="_blank" rel="noreferrer" className="text-dark display-6"><AiFillGithub /></a>}
+              {linkedIn && <a href={linkedIn} target="_blank" rel="noreferrer" className="text-dark display-6"><AiFillLinkedin /></a>}
+              {youTube && <a href={youTube} target="_blank" rel="noreferrer" className="text-dark display-6"><AiFillYoutube /></a>}
+              {website && <a href={website} target="_blank" rel="noreferrer" className="text-dark display-6"><CgWebsite /></a>}
+              {resume && <a href={resume} target="_blank" rel="noreferrer" className="text-dark display-6"><IoDocumentText /></a>}
             </div>
-            <ProjectLikes projectID={projectID}/>
-          </div>
-          <div className="message-area bg-dark text-white shadow rounded mb-3 p-3">messages</div>
-          </div>
+          {/* <ProjectLikes projectID={projectID} /> */}
+          {/* <ul>
+            <li>Projects: 3</li>
+            <li>Likes: 12</li>
+            <li>Comments: 55</li>
+            <li>Favorites: 55</li>
+          </ul> */}
+        </div>
+        <div className="message-area bg-dark text-white rounded mb-3 p-3">
+          messages
+        </div>
+      </div>
     </Container>
   );
 }
-
-

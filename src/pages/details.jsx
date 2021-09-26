1a4625480a154b00f0efded94971b622ee8a74bc
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-09-26 01:30:17 -0500

    refactored likes and avatar (#6)

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -3,10 +3,10 @@ import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Avatar from "../componets/Avatar/avatar";
 import BackButton from "../componets/BackButton";
+import ProjectLikes from "../componets/ProjectLikes/projectLikes";
 
 export default function Details() {
   const { projectID } = useParams();
-
   return (
     <Container>
       <div className="details">
@@ -22,8 +22,13 @@ export default function Details() {
               <Avatar
                 size={175}
                 imgUrl="https://res.cloudinary.com/course-share/image/upload/v1629672423/6153188_57ff4c2bc0_1c63dadd23.jpg"
+                firstName={"Paul"}
+                lastName={"Brats"}
+                showName
               />
+              
             </div>
+            <ProjectLikes projectID={1}/>
           </div>
           <div className="message-area bg-dark text-white shadow rounded mb-3 p-3">messages</div>
           </div>

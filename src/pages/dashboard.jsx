7bda52a70add95b3b34e3ed902d7f53eb31a19e2
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-12-07 21:03:19 -0500

    fixed linting issues

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -7,8 +7,6 @@ import { useHistory } from "react-router-dom";
 import { PROJECTS_QUERY } from "../apollo/queries/projectsQuery";
 import TwoColumns from "../componets/TwoColumns/twoColumns";
 
-const unusedvar = "some text";
-
 const INITIAL_FORM_STATE = {
   name: "",
   description: "",

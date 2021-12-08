8b9b1603ffc40c471be7602d6622c22a025cc748
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-12-07 20:41:35 -0500

    Adding blog (#15)
    * adding a blog
    
    * added basic posts front and back
    
    * added mark down
    
    * bunch of changes:
    
    * huh
    
    * did a think
    
    * adding a blog
    
    * added basic posts front and back
    
    * added mark down
    
    * bunch of changes:
    
    * huh
    
    * did a think

---
+++ src/componets/Table/table.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import TableRow from './tableRow';
+
+const TableStyled = styled.table`
+  display: table;
+  width: 100%;
+  color: #adb5bd;
+  text-align: left;
+  border-collapse: collapse;
+  border-spacing: 1px;
+  margin-bottom: 1.5rem;
+
+  thead {
+    vertical-align: middle;
+  }
+
+  td,
+  th {
+    padding: 0.85rem;
+    white-space: nowrap;
+    
+  }
+
+  tr {
+    display: table-row;
+    vertical-align: bottom;
+    border-bottom: 1px solid #3a4250;
+  }
+`;
+
+export default function Table({ children: columns, sourceData }) {
+  return (
+    <TableStyled>
+      <thead>
+        <tr>
+          <th>#</th>
+          {columns.map((column) => (
+            <th>{column.props.label}</th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {sourceData.map((row, index) => {
+          return <TableRow row={row} columns={columns} index={index} />;  })}
+      </tbody>
+    </TableStyled>
+  );
+}
+
+/*
+
+display: table;
+  width: 100%;
+  color: #adb5bd;
+  text-align: left;
+  border-collapse: collapse;
+  border-spacing: 1px;
+  margin-bottom: 1.5rem;
+
+  thead {
+    vertical-align: middle;
+  }
+
+  td,
+  th {
+    padding: 0.85rem;
+    white-space: nowrap;
+    
+  }
+
+  tr {
+    display: table-row;
+    vertical-align: bottom;
+    border-bottom: 1px solid #3a4250;
+  }
+
+  */

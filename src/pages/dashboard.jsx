0e5d3bb4967e252c0b24f29920c9c7b8287eb92a
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-12-28 18:43:24 -0500

    basic add blog form

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,14 +1,14 @@
 import { Container } from 'react-bootstrap';
 import Pane from '../componets/Pane/pane';
 import AddPost from '../componets/AddPost/addPost';
-// import AddProject from '../componets/AddProject/addProject';
+import AddProject from '../componets/AddProject/addProject';
 
 export default function Dashboard() {
   return (
     <Container>
-        {/* <Pane>
+        <Pane>
           <AddProject />
-        </Pane> */}
+        </Pane>
         <Pane>
           <h2>Work in progress.</h2>
           <p>Working on being able to post markdown blog to strapi.</p>

713971832d4909cb69addef2bdbdf0dccdaca73b
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-01-05 00:00:54 -0500

    show 100 projects

--- src/apollo/queries/projectsQuery.js
+++ src/apollo/queries/projectsQuery.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client"
 
 export const PROJECTS_QUERY = gql`
   query GetAllProjects {
-    projects(limit: 6) {
+    projects(limit: 100) {
       id
       name
       description

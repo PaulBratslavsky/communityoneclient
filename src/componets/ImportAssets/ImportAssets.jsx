10aba13d6e077932ca060368e9e017726db38f66
paul bratslavsky <paulbratslavsky@pauls-MacBook-Pro.local>
2022-01-25 10:20:27 -0500

    csv changes

--- src/componets/ImportAssets/ImportAssets.jsx
+++ src/componets/ImportAssets/ImportAssets.jsx
@@ -1,108 +1,76 @@
 import React, { useMemo, useState, useContext } from "react";
 import Papa from "papaparse";
 import { Form, Button } from "react-bootstrap";
-import { createColumsDef } from '../Grid/utils';
+import { createColumsDef } from "../Grid/utils";
 import { CustomHeader } from "../Grid/CustomHeader";
 import Grid from "../Grid";
-import useContentTypes from '../../hooks/useContentTypes';
-import ImportAssetsUpload from '../ImportAssetsUpload/ImportAssetsUpload';
-import { UserContext } from '../../context/UserContext';
-
+import useContentTypes from "../../hooks/useContentTypes";
+import ImportAssetsUpload from "../ImportAssetsUpload/ImportAssetsUpload";
+import { UserContext } from "../../context/UserContext";
 
 export default function ImportAssets() {
   const { user } = useContext(UserContext);
 
-  const [file, setFile] = React.useState("");
   const [columsDef, setColumsDef] = React.useState([]);
   const [rowData, setRowData] = React.useState([]);
-  const [loading, setLoading] = React.useState(false);
 
-//   const { contentTypes } = useContentTypes();
+  //   const { contentTypes } = useContentTypes();
 
-//  console.log(contentTypes);
+  //  console.log(contentTypes);
 
+  const [analysis, setAnalysis] = useState(null);
+  const [target, setTarget] = useState(null);
+  const [isLoading, setIsLoadig] = useState(false);
 
-const [analysis, setAnalysis] = useState(null);
-const [target, setTarget] = useState(null);
-const [isLoading, setIsLoadig] = useState(false);
+  const analizeImports = async ({ data, type }) => {
+    // Prevent Empty Destination
+    const dataToSend = {
+      data,
+      type,
+    };
 
-const analizeImports = async ({ data, type }) => {
-  // Prevent Empty Destination
-  const dataToSend = {
-    data,
-    type,
-  }
-  
-  // Send Request
-  try {
-    setIsLoadig(true);
-        const response = await fetch("http://localhost:1337/import-assets/preAnalyzeContent", {
+    // Send Request
+    try {
+      setIsLoadig(true);
+      const response = await fetch(
+        "http://localhost:1337/import-assets/preAnalyzeContent",
+        {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${user.token}`,
+            Authorization: `Bearer ${user.token}`,
           },
           body: JSON.stringify(dataToSend),
+        }
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+      // Set Content Type Data to map
+      // setTarget(contentTypes.find(({ uid }) => uid === importDest));
+      setAnalysis(data);
 
-        });
-        const responseData = await response.json();
-        console.log(responseData)
-    // Set Content Type Data to map
-    // setTarget(contentTypes.find(({ uid }) => uid === importDest));
-    setAnalysis(data);
-
-    // Notifications
-    //TODO: set notification to user
-  } catch (error) {
-    console.error(error);
-    //TODO: set notification error to user
-  } finally {
-    setIsLoadig(false);
-  }
-
-};
-
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+      // Notifications
+      //TODO: set notification to user
+    } catch (error) {
+      console.error(error);
+      //TODO: set notification error to user
+    } finally {
+      setIsLoadig(false);
+    }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    Papa.parse(file, {
-      header: true,
-      dynamicTyping: true,
-      complete: function (tableData) {
-        const header = tableData.meta.fields;
-        setColumsDef(createColumsDef(header));
-        setRowData(tableData.data);
-        setLoading(false);
-      },
-    });
-  };
-
-  console.log(rowData)
+  console.log(setAnalysis);
   return (
     <div>
-      <Form onSubmit={handleSubmit}>
-        <fieldset disabled={loading}>
-          <Form.Group controlId="formFile" className="mb-3">
-            <Form.Label>Default file input example</Form.Label>
-            <Form.Control type="file" onChange={handleFileChange} />
-            <Button variant="primary" type="submit">
-              Process CSV
-            </Button>
-          </Form.Group>
-        </fieldset>
-      </Form> 
-
       <ImportAssetsUpload onSubmit={analizeImports} />
 
-      
       <div>
-        <Grid columsDef={columsDef} rowData={rowData} customHeader={CustomHeader} /> 
+        <Grid
+          columsDef={columsDef}
+          rowData={rowData}
+          customHeader={CustomHeader}
+        />
       </div>
-      
     </div>
   );
 }
